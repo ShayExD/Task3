95069f5c6dc46383f8bbe624883f5159033c810b
barleviatias <barlevi.atias.com>
2024-02-25 20:49:20 +0200

    few changes+ admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,43 @@
-import { useState,useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Register from './FComponents/register'
-import Login from './FComponents/login'
-import Profile from './FComponents/profile'
-import EditDetails from './FComponents/editDetails'
-
-
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Register from './FComponents/register';
+import Login from './FComponents/login';
+import Profile from './FComponents/profile';
+import EditDetails from './FComponents/editDetails';
+import SystemAdmin from './FComponents/systemAdmin';
+import usersData from './utils/userData.js';
+// import { users } from './utils/users.json';
 
 function App() {
+	const [user, setUser] = useState(
+		JSON.parse(sessionStorage.getItem('loggedUser')) || ''
+	);
 
-  const loadUsers = () => {
-    const users = JSON.parse(localStorage.getItem('users')) || [];
-    console.log('Users:', users);
-    // You can return users or perform any other operations with the users if needed.
-    return users;
-  };
+	const loadUsers = () => {
+		console.log(usersData);
+		const users = JSON.parse(localStorage.getItem('users')) || usersData;
+		console.log('Users:', users);
+		return users;
+	};
 
-  useEffect(() => {
-    loadUsers();
-  }, []);
+	useEffect(() => {
+		const savedUsers = JSON.parse(localStorage.getItem('users'));
+		if (!savedUsers || savedUsers.length === 0) {
+			localStorage.setItem('users', JSON.stringify(usersData));
+		}
+		loadUsers();
+	}, []);
 
-
-  return (
-    <>
-    <Login/>
-    <Profile/>
-    <Register/>
-    <EditDetails/>
-    </>
-  )
+	return (
+		<>
+			{user === '' ? <Login /> : ''}
+			{user.username === 'admin' ? <SystemAdmin /> : <Profile />}
+			{user === '' ? <Register /> : ''}
+			<EditDetails />
+		</>
+	);
 }
 
-export default App
+export default App;
